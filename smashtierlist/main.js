65baebf5e5101c4b068e8d0a3c01057ae4876fa5
DecklynKern <71661150+DecklynKern@users.noreply.github.com>
2022-01-06 23:19:52 -0700

    add saving

--- smashtierlist/main.js
+++ smashtierlist/main.js
@@ -14,27 +14,29 @@ function drop(ev) {
 
   if(ev.target.parentElement.className == "imagelistelem") {
   	ev.target.parentElement.parentElement.insertBefore(document.getElementById(data), ev.target.parentElement);
-  } else if (ev.target.tagName == "TD" && ev.target.className == "tierslot") {
+  } else if (ev.target.tagName == "DIV" && ev.target.className == "tierslot") {
   	ev.target.children[0].appendChild(document.getElementById(data));
   }
 }
 
 function addRow(text, color) {
 
-	tableBody = document.getElementById("tiertable").tBodies[0];
-	row = document.createElement("tr");
+	tableBody = document.getElementById("tiertable");
+	row = document.createElement("div");
 	row.className = "tierrow";
 	tier = tableBody.children.length;
 	row.id = "tier" + tier;
 
 	row.innerHTML = `
-		<td class="tiertitle" bgcolor="` + color + `" onclick="setCurrentTier('` + tier + `')">
+		<div class="tiertitle" onclick="setCurrentTier('` + tier + `')">
 			` + text + `
-		</td>
-		<td class="tierslot" ondrop="drop(event)" ondragover="allowDrop(event)" onclick="setCurrentTier('` + tier + `')">
+		</div>
+		<div class="tierslot" ondrop="drop(event)" ondragover="allowDrop(event)" onclick="setCurrentTier('` + tier + `')">
 			<ul class="tierlist">
 			</ul>
-		</td>`;
+		</div>`;
+
+	row.children[0].style.backgroundColor = color;
 	tableBody.appendChild(row);
 
 }
@@ -57,11 +59,6 @@ function setCurrentTier(tier) {
 	}
 }
 
-function setText() {
-	tier = document.getElementById("currenttier").value;
-	document.getElementById("tier" + tier).children[0].innerHTML = document.getElementById("nameentry").value;
-}
-
 function setColour(color) {
 	document.getElementById("tier" + document.getElementById("currenttier").value).children[0].style.backgroundColor = color;
 }
@@ -70,6 +67,11 @@ function createImage() {
 	
 	tag = document.getElementById("tagentry").value;
 	char = document.getElementById("charentry").value.trim().toLowerCase().replace(" ", "").replace(".", "").replace("/", "").replace("-", "");
+	addImage(1, tag, char);
+
+}
+
+function addImage(tier, tag, char) {
 
 	aliassedChar = aliasMap.get(char);
 
@@ -85,11 +87,10 @@ function createImage() {
 		img.id = tag + Math.random();
 		img.className = "imagelistelem"
 		img.innerHTML = `
-			<img id="` + path + img.id + `" class="char-image" src = "` + path + `" draggable="true" ondragstart="drag(event)" onmouseover="setDeleteButtonVisibility(this, true)" onmouseout="setDeleteButtonVisibility(this, false)">
+			<img class="char-image" src = "` + path + `" draggable="true" ondragstart="drag(event)" onmouseover="setDeleteButtonVisibility(this, true)" onmouseout="setDeleteButtonVisibility(this, false)">
 			<img class="deleteimagebutton" src="deletebutton.png" onclick="deleteImage(this)" onmouseover="setDeleteButtonVisibility(this, true)">
 			<p class="tagtext">` + tag + `</p>`
-		document.getElementById("tier1").children[1].children[0].appendChild(img);
-	} else {
+		document.getElementById("tier" + tier).children[1].children[0].appendChild(img);
 	}
 }
 
@@ -103,4 +104,61 @@ function setDeleteButtonVisibility(img, visible) {
 
 function deleteImage(button) {
 	button.parentElement.remove();
+}
+
+function loadParams() {
+
+	obj = JSON.parse(atob(new URLSearchParams(window.location.search).get("d")));
+
+	for(i = 0; i < obj["rows"].length; i++) {
+
+		row = obj["rows"][i];
+		rowTitle = row[0];
+		color = row[1];
+
+		if(i == document.getElementById("tiertable").children.length - 1) {
+			addRow(rowTitle, color);
+			tierRow = document.getElementById("tiertable").children[i + 1];
+
+		} else {
+			tierRow = document.getElementById("tiertable").children[i + 1];
+			tierRow.children[0].style.backgroundColor = color
+			tierRow.children[0].innerHTML = rowTitle;
+		}
+
+		for(j = 0; j < row[2].length; j++) {
+			console.log(document.getElementById("tiertable").children[i + 1]);
+			addImage(i + 1, row[2][j][0], row[2][j][1]);
+		}
+
+	}
+
+}
+
+function createLink() {
+
+	url = window.location.href.split("?")[0];
+	obj = {"rows": []};
+
+	for(i = 1; i < document.getElementById("tiertable").children.length; i++) {
+
+		row = document.getElementById("tiertable").children[i];
+		rowTitle = row.children[0].innerHTML.trim();
+
+		obj["rows"].push([rowTitle, row.children[0].style.backgroundColor, []]);
+
+		for(j = 0; j < row.children[1].children[0].children.length; j++) {
+			image = row.children[1].children[0].children[j];
+			tag = image.children[2].innerHTML;
+			imgUrl = image.children[0].src.split("/");
+			char = imgUrl[imgUrl.length - 1].split(".")[0];
+			obj["rows"][i - 1][2].push([tag, char]);
+		}
+
+	}
+
+	string = btoa(JSON.stringify(obj));
+
+	console.log(obj);
+	console.log(url + "?d=" + string);
 }
