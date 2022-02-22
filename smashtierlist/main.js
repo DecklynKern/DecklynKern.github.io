190c1a850c9c87ae1e4bcbef4060a91cc5adc126
DecklynKern <71661150+DecklynKern@users.noreply.github.com>
2022-02-22 13:28:02 -0700

    almost done!

--- smashtierlist/main.js
+++ smashtierlist/main.js
@@ -78,7 +78,9 @@ function setCurrentTier(tier) {
 
 	if(oldTier != tier) {
 		document.getElementById("currenttier").value = tier;
-		document.getElementById("tier" + tier).children[1].style.backgroundColor = "#555";
+		tier = document.getElementById("tier" + tier);
+		tier.children[1].style.backgroundColor = "#555";
+		document.getElementById("colorselector").value = "#" + colourToHex(tier.children[0].style.backgroundColor);
 
 	} else {
 		document.getElementById("currenttier").value = "0";
@@ -87,12 +89,18 @@ function setCurrentTier(tier) {
 
 function setColour(color) {
 	document.getElementById("tier" + document.getElementById("currenttier").value).children[0].style.backgroundColor = color;
+	hex = colourToHex(color);
+	if(hex.startsWith("#")){
+		document.getElementById("colorselector").value = hex;
+	} else{
+		document.getElementById("colorselector").value = "#" + hex;
+	}
 }
 
 function createImage() {
 	
 	tag = document.getElementById("tagentry").value;
-	char = document.getElementById("charentry").value.trim().toLowerCase().replace(" ", "").replace(".", "").replace("/", "").replace("-", "");
+	char = document.getElementById("charentry").value.trim().toLowerCase().replaceAll(" ", "").replaceAll("&", "and").replaceAll(".", "").replaceAll("/", "").replaceAll("-", "");
 	addImage(0, tag, char);
 }
 
@@ -116,7 +124,7 @@ function addImage(tier, tag, char) {
 
 		img.innerHTML = `
 			<img class="char-image" src = "` + path + `" draggable="true" ondragstart="drag(event)" onmouseover="setDeleteButtonVisibility(this, true)" onmouseout="setDeleteButtonVisibility(this, false)">
-			<img class="deleteimagebutton" src="deletebutton.png" onclick="deleteImage(this)" onmouseover="setDeleteButtonVisibility(this, true)">
+			<img class="deleteimagebutton" src="deletebutton.png" onclick="deleteImage(this)" onmouseover="setDeleteButtonVisibility(this, true)" onmouseout="setDeleteButtonVisibility(this, false)">
 			<p class="tagtext">` + tag + `</p>`
 
 		if(tier == 0){
@@ -141,28 +149,54 @@ function deleteImage(button) {
 	button.parentElement.remove();
 }
 
+function colourToHex(rgb) {
+
+	split = rgb.split(",");
+
+	if(split.length == 1){
+		return split[0];
+
+	} else{
+		
+		r = parseInt(split[0].split("(")[1]).toString(16);
+		g = parseInt(split[1]).toString(16);
+		b = parseInt(split[2].split(")")[0]).toString(16);
+
+		if(r.length == 1) {r = "0" + r};
+		if(g.length == 1) {g = "0" + g};
+		if(b.length == 1) {b = "0" + b};
+
+		return r + g + b;
+	}
+}
+
 function loadParams() {
 
 	obj = JSON.parse(atob(new URLSearchParams(window.location.search).get("d")));
 
-	for(i = 0; i < obj["rows"].length; i++) {
+	for(i = 0; i < obj[0].length; i++) {
+		addImage(0, obj[0][i][0], characterNames[obj[0][i][1]]);
+	}
 
-		r = obj["rows"][i];
+	for(i = 1; i < obj.length; i++) {
+
+		r = obj[i];
 		rowTitle = r[0];
 		color = r[1];
 
-		if(i == document.getElementById("tiertable").children.length - 1) {
+		if(i == document.getElementById("tiertable").children.length) {
 			addRow(rowTitle, color);
-			tierRow = document.getElementById("tiertable").children[i + 1];
+			tierRow = document.getElementById("tiertable").children[i];
 
 		} else {
-			tierRow = document.getElementById("tiertable").children[i + 1];
-			tierRow.children[0].style.backgroundColor = color
+			tierRow = document.getElementById("tiertable").children[i];
+			tierRow.children[0].style.backgroundColor = color;
+			tierRow.children[0].style.backgroundColor = "#" + color;
 			tierRow.children[0].innerHTML = rowTitle;
 		}
 
 		for(j = 0; j < r[2].length; j++) {
-			addImage(i + 1, r[2][j][0], r[2][j][1]);
+			addImage(i, r[2][j][0], characterNames[r[2][j][1]]);
 		}
 	}
 }
@@ -170,25 +204,36 @@ function loadParams() {
 function createLink() {
 
 	url = window.location.href.split("?")[0];
-	obj = {"rows": []};
+	obj = [[]];
+
+	sidelist = document.getElementById("createdimagelist").children[0];
+
+	for(i = 0; i < sidelist.children.length; i++) {
+		image = sidelist.children[i];
+		tag = image.children[2].innerHTML;
+		imgUrl = image.children[0].src.split("/");
+		char = characterNames.indexOf(imgUrl[imgUrl.length - 1].split(".")[0]);
+		obj[0].push([tag, char]);
+	}
 
 	for(i = 1; i < document.getElementById("tiertable").children.length; i++) {
 
 		row = document.getElementById("tiertable").children[i];
 		rowTitle = row.children[0].innerHTML.trim();
 
-		obj["rows"].push([rowTitle, row.children[0].style.backgroundColor, []]);
+		obj.push([rowTitle, colourToHex(row.children[0].style.backgroundColor), []]);
 
 		for(j = 0; j < row.children[1].children[0].children.length; j++) {
 			image = row.children[1].children[0].children[j];
 			tag = image.children[2].innerHTML;
 			imgUrl = image.children[0].src.split("/");
-			char = imgUrl[imgUrl.length - 1].split(".")[0];
-			obj["rows"][i - 1][2].push([tag, char]);
+			char = characterNames.indexOf(imgUrl[imgUrl.length - 1].split(".")[0]);
+			obj[i][2].push([tag, char]);
 		}
 	}
 
-	string = btoa(JSON.stringify(obj));
+	string = btoa(JSON.stringify(obj)).replaceAll("=", "");
+	console.log(JSON.stringify(obj));
 
 	document.getElementById("linkbox").innerHTML = url + "?d=" + string;
 }
