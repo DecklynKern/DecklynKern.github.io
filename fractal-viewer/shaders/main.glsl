b8bcf43ff362d2118fb5286ebb6371be090019fd
DecklynKern <decklynkern@gmail.com>
2023-10-27 15:44:06 -0600

    Update main.glsl

--- fractal-viewer/shaders/main.glsl
+++ fractal-viewer/shaders/main.glsl
@@ -160,8 +160,17 @@ vec3 lab2rgb(vec3 c) {
     return xyz2rgb( lab2xyz( vec3(100.0 * c.x, 2.0 * 127.0 * (c.y - 0.5), 2.0 * 127.0 * (c.z - 0.5)) ) );
 }
 
-vec3 hsv2rgb(vec3 c)
-{
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
     return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
@@ -215,6 +224,9 @@ void main() {
 
         #elif MULTISAMPLING_ALGORITHM == 3
             pixel_sample = rgb2lab(pixel_sample);
+
+        #elif MULTISAMPLING_ALGORITHM == 4
+            pixel_sample = rgb2hsv(pixel_sample);
         #endif
 
         colour_sum += pixel_sample;
@@ -235,6 +247,9 @@ void main() {
 
     #elif MULTISAMPLING_ALGORITHM == 3
         final_colour = lab2rgb(colour_sum);
+
+    #elif MULTISAMPLING_ALGORITHM == 4
+        final_colour = hsv2rgb(colour_sum);
     #endif
 
     colour = vec4(final_colour, 1.0);
